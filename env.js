e4fd8ec0ed8964246cff36de467b0976b531b933
BeratKoc <abk301@hotmail.com>
2025-12-12 01:01:59 +0300

    env

--- env.js
+++ env.js
@@ -4,7 +4,7 @@
 window.MAIN_STYLE_URL = "style.css";               // index.html için
 window.API_BASE_URL = "https://<neoprint>.railway.app";
 
-window.OFFER_STYLE_URL   = "offer/offer.css";
-window.OFFER_JS_URL      = "offer/offer.js";
-window.PAYMENT_STYLE_URL = "payment/payment.css";
-window.PAYMENT_JS_URL    = "payment/payment.js";
+window.OFFER_STYLE_URL   = "offer.css";
+window.OFFER_JS_URL      = "offer.js";
+window.PAYMENT_STYLE_URL = "payment.css";
+window.PAYMENT_JS_URL    = "payment.js";
