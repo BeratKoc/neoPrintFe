f510f491e5a0378268bab206749bdf37a73247c8
unknown <abk301@hotmail.com>
2025-12-11 20:41:28 +0300

    v1.0

---
+++ env.prod.js
@@ -0,0 +1,7 @@
+window.API_URL = "/api";
+
+// CSS dosyaları
+window.MAIN_STYLE_URL = "style.min.css";           // index.html için
+window.OFFER_STYLE_URL = "./offer.min.css";        // offer.html için (relative!)
+
+window.OFFER_JS_URL = "./offer.min.js"; // local test için
